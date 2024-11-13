e282c41b194019cca0d81ab5d49cd50beffd8739
STRIXIE\freddie <frederickfreund@gmail.com>
2024-11-13 16:00:27 +0100

    Synchronize cycle state switching

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -1,48 +1,86 @@
 using System;
+using System.Collections;
 using Input;
+using Unity.Netcode;
 using UnityEngine;
 
-public class DayNightCycle : MonoBehaviour
+public class DayNightCycle : NetworkBehaviour
 {
     [SerializeField] private InputReader inputReader;
 
+    private NetworkVariable<float> _turnTime;
     private CycleState _cycleState = CycleState.Night;
 
-    private void OnEnable()
+    public override void OnNetworkSpawn()
     {
-        inputReader.OnSwitchDayNightCycle += SwitchCycleState;
+        if (IsClient)
+            _turnTime.OnValueChanged += HandleTurnTimeChanged;
+            
+        if (IsServer)
+        {
+            StartCoroutine(TimerRoutine());
+            inputReader.OnSwitchDayNightCycle += SwitchCycleState;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+
+        if (IsServer)
+        {
+            inputReader.OnSwitchDayNightCycle -= SwitchCycleState;
+        }
     }
     
-    private void OnDisable()
-    {
-        inputReader.OnSwitchDayNightCycle -= SwitchCycleState;
-    }
+    #region Server
 
     private void SwitchCycleState()
     {
         if (_cycleState == CycleState.Day)
         {
-            SwitchToNightTime();
+            SwitchToNightTimeClientRpc();
         }
         else if(_cycleState == CycleState.Night)
         {
-            SwitchToDayTime();
+            SwitchToDayTimeClientRpc();
         }
     }
-    
-    private void SwitchToDayTime()
+
+    private IEnumerator TimerRoutine()
+    {
+        while (true)
+        {
+            _turnTime.Value += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    #endregion
+
+    #region Client
+
+    [ClientRpc]
+    private void SwitchToDayTimeClientRpc()
     {
         Debug.Log("SwitchToDayTime");
         _cycleState = CycleState.Day;
         GameEvents.DAY_NIGHT_CYCLE.OnSwitchedCycleState?.Invoke(_cycleState);
     }
 
-    private void SwitchToNightTime()
+    [ClientRpc]
+    private void SwitchToNightTimeClientRpc()
     {
         Debug.Log("SwitchToNightTime");
         _cycleState = CycleState.Night;
         GameEvents.DAY_NIGHT_CYCLE.OnSwitchedCycleState?.Invoke(_cycleState);
     }
+    
+    private void HandleTurnTimeChanged(float previousvalue, float newvalue)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
 
     public enum CycleState
     {
