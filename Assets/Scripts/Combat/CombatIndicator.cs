b0bbe3e2d69aea815e470debc1bdc371a6be3d14
STRIXIE\freddie <frederickfreund@gmail.com>
2024-11-19 19:24:00 +0100

    Add combat indicator prefab

---
+++ Assets/Scripts/Combat/CombatIndicator.cs
@@ -0,0 +1,58 @@
+using System;
+using DG.Tweening;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Combat
+{
+    public class CombatIndicator : NetworkBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform combatCircle;
+        
+        [Header("Settings")]
+        [SerializeField] private float radius;
+        [SerializeField] private float showTime;
+        [SerializeField] private float hideTime;
+
+        private Tween _animationTween;
+
+        #region Server
+
+        public void ShowForAll()
+        {
+            ShowClientRpc();
+        }
+
+        public void HideForAll()
+        {
+            HideClientRpc();
+        }
+
+        #endregion
+
+        #region Client
+
+        [ClientRpc]
+        private void ShowClientRpc()
+        {
+            _animationTween?.Kill();
+            combatCircle.localScale = new Vector3(0, combatCircle.localScale.y, 0);
+            _animationTween = combatCircle
+                .DOScale(new Vector3(radius * 0.5f, combatCircle.localScale.y, radius * 0.5f), showTime)
+                .SetEase(Ease.OutBack);
+        }
+
+        [ClientRpc]
+        private void HideClientRpc()
+        {
+            _animationTween?.Kill();
+            combatCircle.localScale = new Vector3(radius * 0.5f, combatCircle.localScale.y, radius * 0.5f);
+            _animationTween = combatCircle
+                .DOScale(new Vector3(0, combatCircle.localScale.y, 0), hideTime)
+                .SetEase(Ease.InBack);
+        }
+
+        #endregion
+    }
+}
