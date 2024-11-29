5b748e0a7c2bd1bc8ddedfc5dd28195570b61cc1
PetschMarvin <marvin.petsch@hacker-school.de>
2024-11-29 23:28:38 +0100

    implemented mountains and forests not spawning on player hexes

--- Assets/Scripts/ExtensionMethods/VectorExtensions.cs
+++ Assets/Scripts/ExtensionMethods/VectorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using HexSystem;
+using UnityEngine;
 
 namespace ExtensionMethods
 {
@@ -7,6 +8,11 @@ namespace ExtensionMethods
         public static Vector3 Rotate(this Vector3 vector, float degree, Vector3 axis)
         {
             return Quaternion.AngleAxis(degree, axis) * vector;
+        }
+
+        public static AxialCoordinates ToAxialCoordinates(this Vector2Int vector)
+        {
+            return new AxialCoordinates(vector.x, vector.y);
         }
     }
 }
