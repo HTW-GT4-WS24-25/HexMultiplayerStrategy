f0eb0da782692f5db41c1c22033fcbf5a4aa2d25
Freddie <frederickfreund@gmail.com>
2024-12-17 21:05:01 +0100

    Fix SinglePlayerTestScene

--- Assets/Scripts/Testing/QuickSinglePlayerSetup/TestSinglePlayerColorSetter.cs
+++ Assets/Scripts/Testing/QuickSinglePlayerSetup/TestSinglePlayerColorSetter.cs
@@ -9,12 +9,14 @@ namespace Testing.QuickSinglePlayerSetup
     public class TestSinglePlayerColorSetter : MonoBehaviour
     {
         [SerializeField] PlayerColor.ColorType playerColorType = PlayerColor.ColorType.Red;
+        [SerializeField] UnitModel.ModelType modelType = UnitModel.ModelType.Rabbit;
         
         private const string GameSceneName = "MatchScene";
         
         void Start()
         {
             HostSingleton.Instance.GameManager.PlayerData.SetPlayerColorType(NetworkManager.Singleton.LocalClientId, playerColorType);
+            HostSingleton.Instance.GameManager.PlayerData.SetPlayerFaction(NetworkManager.Singleton.LocalClientId, modelType);
             NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
         }
     }
