8b0a0349bdcb27a7cc62cc72caa269f48c417d46
Freddie <frederickfreund@gmail.com>
2024-12-17 20:40:22 +0100

    Remove magic numbers from UnitAnimator

--- Assets/Scripts/HexTypeDataProvider.cs
+++ Assets/Scripts/HexTypeDataProvider.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Sirenix.OdinInspector;
-using Sirenix.Serialization;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HexTypeDataProvider : MonoBehaviour
 {
